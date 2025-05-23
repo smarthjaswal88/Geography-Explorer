c87fc338b0bd2e8a848c9a2a003a24610d0621f9
Smarth Jaswal <121184151+smarthjaswal88@users.noreply.github.com>
2025-05-23 18:02:13 +0530

    Update HomePage.js

--- oca-client/src/Components/HomePage/HomePage.js
+++ oca-client/src/Components/HomePage/HomePage.js
@@ -1,8 +1,6 @@
 import React from "react";
 import LogoutButton from "../Logout/LogoutButton";
 import "./HomePage.css";
-import PakistanMap from "../PakMap/PakisthanMap";
-import IndiaMap from "../IndiaMap/IndiaMap";
 import DynamicMapViewer from "../DynamicMapViewer/DynamicMapViewer";
 
 function HomePage() {
